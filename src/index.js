0cc789fea073f834e0fd8dba4e4bbb1f4c23e8cf
Justin Daniel <justinddaniel@gmail.com>
2018-08-17 15:54:46 -0400

    Write first component

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Create a new component and this component should produce some HTML. 
+// Take this component's generated HTML and put it in the DOM
+
+const App = function () {
+	return <div> Hi! </div>;
+}
+
