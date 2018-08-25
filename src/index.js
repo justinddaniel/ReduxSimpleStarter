9d971907e71e9b3ce404d934c1c9dda617e3a49f
Justin Daniel <justinddaniel@gmail.com>
2018-08-25 13:48:30 -0400

    Wired up to Youtube API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyCtQpNd1UkmDVriawjY-_nKu2j1nJLBZO0';
+
 // Create a new component and this component should produce some HTML.
 
 const App = () => {
 	return <div> Hi! </div>;
 }
 
-//always use one component per file. 
+//always use one component per file.
 
 // Take this component's generated HTML and put it in the DOM
 ReactDOM.render(<App />, document.querySelector(".container"))
