c4835eba0f2d59df96edd9fc887dddbe9656ce12
Justin Daniel <justinddaniel@gmail.com>
2018-08-30 15:01:47 -0400

    Make API call to youtube from index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCtQpNd1UkmDVriawjY-_nKu2j1nJLBZO0';
 
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+	console.log(data);
+})
+
 // Create a new component and this component should produce some HTML.
+
+//downwards data flow: only the most parent component should be responsible for fetching data.
 
 const App = () => {
 	return  (
