10ff4d7716d7667f51a912cab0d707440590d39f
Justin Daniel <justinddaniel@gmail.com>
2018-08-30 15:18:47 -0400

    Export and import VideoList

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyCtQpNd1UkmDVriawjY-_nKu2j1nJLBZO0';
 
 
@@ -26,6 +27,7 @@ class App extends Component {
 		return  (
 			<div>
 				<SearchBar />
+				<VideoList />
 			</div>
 		);
 	}
