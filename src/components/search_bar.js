bb542f4b775b741c1c6841a914d1f64438f52818
Justin Daniel <justinddaniel@gmail.com>
2018-08-30 13:43:17 -0400

    Refactor to ES6

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react'; //imort React, pull off property Component as variable Component
 
 
-class SearchBar extends React.Component {
+class SearchBar extends Component {
     render() { //every React class must have a render method
-
+      return <input />;
     }
 }
 
