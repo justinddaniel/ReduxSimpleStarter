47cb2a28586094b57302c758a1f84a9da4e9e011
Justin Daniel <justinddaniel@gmail.com>
2018-08-30 15:05:44 -0400

    convert App to class component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyCtQpNd1UkmDVriawjY-_nKu2j1nJLBZO0';
 
 YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
@@ -13,12 +12,14 @@ YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
 
 //downwards data flow: only the most parent component should be responsible for fetching data.
 
-const App = () => {
-	return  (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	render() {
+		return  (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
 
 //always use one component per file.
