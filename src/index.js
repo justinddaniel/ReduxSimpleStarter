abfdc0399a14700de94d33046aa0b0c6d94dc5e6
Justin Daniel <justinddaniel@gmail.com>
2018-09-01 15:06:19 -0400

    installed and imported lodash

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+import _ from 'lodash';
 const API_KEY = 'AIzaSyCtQpNd1UkmDVriawjY-_nKu2j1nJLBZO0';
 
 
