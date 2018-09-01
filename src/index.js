6d8b0ad689e08903febecd717f2b928d6ea2cd6b
Justin Daniel <justinddaniel@gmail.com>
2018-09-01 14:29:03 -0400

    begin wiring selectedVideo state

--- src/index.js
+++ src/index.js
@@ -16,7 +16,10 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = {
+			videos: [],
+		 	selectedVideo: null
+		};
 
 		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
 			this.setState({ videos }); //key and value both videos so can condense
@@ -28,7 +31,7 @@ class App extends Component {
 		return  (
 			<div>
 				<SearchBar />
-				<VideoDetail video={this.state.videos[0]}/>
+				<VideoDetail video={this.state.selectedVideo}/>
 				<VideoList videos={this.state.videos} />
 			</div> // passing props with videos (prop videos)
 		);
